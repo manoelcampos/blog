94816df3b9f301616429b64f3b3a698c8808af40
Manoel Campos <manoelcampos@gmail.com>
2016-05-05 01:13:43 +0100

    Adicionado script para publicar blog no Amazon S3

---
+++ src/removerdata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Exclui o campo date: do cabeçalho dos arquivos markdown pois ao usar o Spress para gerar o site, ele estava reclamando de tal campo."
+for f in content/posts/*.md ; do 
+	awk '!/date: /' "$f"  > temp && mv temp "$f" ; 
+done
+rm -f temp
